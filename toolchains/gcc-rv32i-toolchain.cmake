3b20ce2b733172715ef1495ca528c4e70f112d69
Stephan Creutz <stephan.cr@gmx.de>
2023-12-24 14:45:14 +0100

    fix compilation for "v" target environment

--- toolchains/gcc-rv32i-toolchain.cmake
+++ toolchains/gcc-rv32i-toolchain.cmake
@@ -2,4 +2,13 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR riscv32)
 
 set(CMAKE_ASM_COMPILER riscv64-elf-gcc)
-set(CMAKE_ASM_FLAGS "-mabi=ilp32 -march=rv32i -nostdlib -mno-relax")
+set(CMAKE_ASM_FLAGS "-mabi=ilp32 -march=rv32i -nostartfiles -mno-relax")
+
+set(CMAKE_C_COMPILER riscv64-elf-gcc)
+set(CMAKE_C_FLAGS "-mabi=ilp32 -march=rv32i -mno-relax -nostartfiles -nodefaultlibs")
+
+set(CMAKE_EXE_LINKER_FLAGS "-Wl,--no-warn-rwx-segments")
+
+# paths to newlib
+include_directories(SYSTEM /usr/riscv32-elf/include)
+link_directories(/usr/riscv32-elf/lib)
