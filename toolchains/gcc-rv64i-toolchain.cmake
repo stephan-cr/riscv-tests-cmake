8453cec353099dcf7b8a9a4fcb1caa84dee279dc
Stephan Creutz <stephan.cr@gmx.de>
2023-12-25 21:01:25 +0100

    fix compilation for the "v" test environment for rv64

--- toolchains/gcc-rv64i-toolchain.cmake
+++ toolchains/gcc-rv64i-toolchain.cmake
@@ -2,4 +2,9 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR riscv64)
 
 set(CMAKE_ASM_COMPILER riscv64-elf-gcc)
-set(CMAKE_ASM_FLAGS "-mabi=lp64 -march=rv64i -nostdlib -mno-relax")
+set(CMAKE_ASM_FLAGS "-mabi=lp64 -march=rv64imac -nostartfiles -mno-relax -mcmodel=medany")
+
+set(CMAKE_C_COMPILER riscv64-elf-gcc)
+set(CMAKE_C_FLAGS "-mabi=lp64 -march=rv64imac -mno-relax -nostartfiles -nodefaultlibs -mcmodel=medany")
+
+set(CMAKE_EXE_LINKER_FLAGS "-Wl,--no-warn-rwx-segments")
